0f2ffea6f214109965e69b73a92db5e464d3114c
brizism <montielbriseida@gmail.com>
2018-02-14 17:49:37 -0500

    post req ajax

--- ajax/app.js
+++ ajax/app.js
@@ -1,6 +1,23 @@
 const http = new easyHTTP;
 
 // Get Posts
-http.get('http://jsonplaceholder.typicode.com/posts', function(err, response){
-  err ? console.log(err) : console.log(response);
+// http.get('http://jsonplaceholder.typicode.com/posts', function(err, response){
+//   err ? console.log(err) : console.log(response);
+// })
+
+// Get Single Post
+// http.get('http://jsonplaceholder.typicode.com/posts/2', function(err, post){
+//   err ? console.log(err) : console.log(post);
+// })
+
+
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// Create Post
+http.post('http://jsonplaceholder.typicode.com/posts', data, function(err, post){
+  err ? console.log(err) : console.log(post);
 })

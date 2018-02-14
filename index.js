e48dfb5d353572bfc75dfd493ec9fa8027eb547d
brizism <montielbriseida@gmail.com>
2018-02-13 23:30:38 -0500

    day 2

--- index.js
+++ index.js
@@ -121,3 +121,33 @@ const peoples = [
       ...comments.slice(0, findId),
       ...comments.slice(findId + 1)
   ];
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+console.clear()
+
+const companies= [
+    {name: "Company One", category: "Finance", start: 1981, end: 2004},
+    {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+    {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+    {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+    {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+    {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+    {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+    {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+    {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+  ];
+  
+  const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+  // forEach
+//   companies.forEach(company => console.log(company))
+
+  // filter
+  const canDrink = ages.filter(age => age >= 18);
+  
+  const retail = companies.filter(category => category.category == 'Retail');
+  
+  const eighties = companies.filter(company => company.start >= 1980);
+  console.log(eighties)
